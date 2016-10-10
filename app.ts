ab46fcd0088d1180e4460ba6e9ecefc84e18fe9c
Yi Li <yl2326@cornell.edu>
2016-10-09 17:10:45 -0700

    added the initial structure and files

---
+++ app.ts
@@ -0,0 +1,22 @@
+﻿module Namespace {
+    export class Game extends Phaser.Game {
+
+        constructor() {
+            super({
+                width: 900,
+                height: 1600,
+                renderer: Phaser.AUTO
+            });
+
+            //this.state.add('game', State.Game);
+            //this.state.add('instructions', State.Instructions);
+            //this.state.add('credits', State.Credits);
+            //this.state.add('mainmenu', State.MainMenu);
+            //this.state.add('preloader', State.Preloader, true);
+        }
+    }
+}
+
+// export Game to window
+var Game = Namespace.Game;
+var loadGame = new Game();
